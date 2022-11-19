419bae7a768844176826262705849a33061e1736
MalekMohanna <malekmhana@gmail.com>
2022-11-19 18:36:01 +0200

    First commit

---
+++ SparkPlanner/src/main/java/com/project/group/SparkPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.project.group;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparkPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SparkPlannerApplication.class, args);
+	}
+
+}
---
+++ SparkPlanner/src/test/java/com/project/group/SparkPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.group;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SparkPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
